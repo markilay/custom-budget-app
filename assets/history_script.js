8e6887fef21d714e21e7d158b97bedc7ad539068
mpometun <mpometun@corp.uber.com>
2020-05-28 13:53:11 +0200

    new history style

--- assets/history_script.js
+++ assets/history_script.js
@@ -2,22 +2,68 @@ const months = ['January', 'February', 'March', 'April', 'May',
     'June', 'July', 'August', 'September', 'October', 'November', 'December'
 ];
 
-const savedMonth = [];
+let savedMonth = [];
 const saveButton = document.querySelector('button')
 const history = document.querySelector('.history')
+const modalOuter = document.querySelector('.modal-outer')
+const modalInner = document.querySelector('.modal-inner')
 
 
 function addMonth() {
     const now = new Date();
     const month = months[now.getMonth()];
     const year = now.getFullYear();
-    savedMonth.push(`${month} ${year}`)
+    if (savedMonth.length === 0 || !(savedMonth[month - 1] == savedMonth[month])) {
+        let item = {
+            month,
+            year,
+            income: 5000,
+            total: 4500,
+        }
+        savedMonth.push(item)
+        localStorage.setItem('history', JSON.stringify(savedMonth))
+        history.innerHTML = savedMonth.map(generateMonthsHTML).join("")
+    }
+
+}
+
+function generateMonthsHTML({
+    month,
+    year
+}, id) {
+    return `<li id="${id}">${month} ${year}</li>`
+}
+
+function showMonthAnalysis(item) {
+    modalInner.innerHTML = `
+    <h3>${item["month"]} ${item["year"]}</h>
+    <p>Income for this Month: ${item.income}</p>
+    <p>Spent Total: ${item.total}</p>
+    `
+    modalOuter.classList.add('open')
+    modalInner.classList.add('open')
+}
+
+
+function restoreFromLocalStorage() {
+    savedMonth = JSON.parse(localStorage.getItem('history')) || []
     history.innerHTML = savedMonth.map(generateMonthsHTML).join("")
 
 }
 
-function generateMonthsHTML(name) {
-    return `<li>${name}</li>`
+function closeModalWindow(e) {
+    if (e.currentTarget === e.target) {
+        modalInner.classList.remove('open')
+        modalOuter.classList.remove('open')
+    }
 }
 
-saveButton.addEventListener('click', addMonth)
+saveButton.addEventListener('click', addMonth);
+history.addEventListener('click', function (e) {
+    savedMonth.forEach((month, id) => id == +e.target.id ? showMonthAnalysis(month) : console.log('wrong month'))
+})
+
+modalOuter.addEventListener('click', closeModalWindow)
+
+
+restoreFromLocalStorage()
